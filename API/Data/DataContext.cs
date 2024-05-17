fca2f4fc65bde18cb214b55a2274b8562f60d621
Trapilaos <nnnikospagidas@gmail.com>
2024-05-17 20:02:19 +0300

    First Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    
+    public string UserName { get; set; }
+    
+
+}
